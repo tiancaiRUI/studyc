33e6a0427d3dd8252060a8b263e1a839d849ed2a
Rui <studyzxr@163.com>
2025-05-05 17:45:54 +0800

    secend try

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -3,7 +3,26 @@
 
 #include "CMakeProject1.h"
 
+template<typename T>
+std::string to_string_helper(T&& arg) {
+	std::ostringstream oss;
+	// 万能引用 接原样转发
+	oss << std::forward<T>(args);
+	return oss.str();
+}
 
+class Logque {
+public:
+	void push();
+	bool pop();
+	void shutdown();
+private:
+	std::queue<std::string> queue_;
+	std::mutex mutex_;
+	std::condition_variable cond_var_;
+	bool is_shutdown_ = false;
+
+};
 
 int main()
 {
