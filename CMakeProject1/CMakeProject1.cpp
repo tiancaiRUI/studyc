5799d112465093cc8ab28f64aba62e3fd63f20cf
Rui <studyzxr@163.com>
2025-05-05 16:41:11 +0800

    first commit

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ CMakeProject1/CMakeProject1.h
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <condition_variable>
+#include <thread>
+#include <fstream>
+// TODO: 在此处引用程序需要的其他标头。
